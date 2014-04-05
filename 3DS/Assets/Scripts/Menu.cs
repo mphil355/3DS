2a9420ec916720dfcfc59f704d21ab84d3505e71
Matt Phillips <mphil355@gmail.com>
2014-04-05 15:20:23 -0500

    menu scene

---
+++ 3DS/Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour 
+{
+	void OnGUI()
+	{
+		if(GUI.Button(new Rect(10, Screen.height/2 - 100, Screen.width/4, Screen.height/10), "Start"))
+		{
+			Application.LoadLevel("Level");
+		}
+		if(GUI.Button(new Rect(10, Screen.height/2, Screen.width/4, Screen.height/10), "Options"))
+		{
+			print ("options");
+		}
+		if(GUI.Button(new Rect(10, Screen.height/2 + 100, Screen.width/4, Screen.height/10), "Exit"))
+		{
+			print ("exit");
+		}
+	}
+}
