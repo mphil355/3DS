4d1e4c2e06adf39a16f9bc75310702919c307222
Matt Phillips <mphil355@gmail.com>
2014-04-05 16:16:38 -0500

    modified number prefabs, fixed launcher

--- 3DS/Assets/SymbolLauncher.cs
+++ 3DS/Assets/SymbolLauncher.cs
@@ -3,26 +3,51 @@ using System.Collections;
 
 public class SymbolLauncher : MonoBehaviour {
 
-	public GameObject referencePoint;
-
 	private Vector3 originalSymbolPosition;
-
+	private int currentIndex;
+	private GameObject current;
 	private bool launchInProgress;
+	private bool canSpawn;
+	private float spawnTimer;
+	private bool symbolInLauncher;
 
+	public float spawnRate;
 	public int maxDistance;
 	public float forceConstant;
 
+	public GameObject[] symbols;
+
+
 	void Start () 
 	{
-		originalSymbolPosition = gameObject.transform.position;
+		spawnTimer = 0;
+		canSpawn = false;
+		originalSymbolPosition = transform.position;
+		currentIndex = 0;
+		SpawnMathSymbol();
 	}
 
 	void Update () 
 	{
-		if (Input.GetMouseButton(0))
-			LaunchBegin();
-		if (Input.GetMouseButtonUp(0) || !Input.GetMouseButton(0) && launchInProgress )
-			LaunchEnd();
+		if(current != null)
+		{
+			if (Input.GetMouseButton(0))
+				LaunchBegin();
+			if (Input.GetMouseButtonUp(0) || !Input.GetMouseButton(0) && launchInProgress )
+				LaunchEnd();
+		}
+		if(!canSpawn && !symbolInLauncher)
+		{
+			spawnTimer += Time.deltaTime;
+			if(spawnTimer >= spawnRate)
+			{
+				canSpawn = true;
+				spawnTimer = 0;
+			}
+		}
+		//print ("spawn: " + canSpawn + "launch: " + launchInProgress);
+		if(canSpawn && !symbolInLauncher)
+			SpawnMathSymbol();
 	}
 
 	void LaunchBegin ()
@@ -31,23 +56,35 @@ public class SymbolLauncher : MonoBehaviour {
 		Vector3 launcherPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		launcherPosition = new Vector3(launcherPosition.x, launcherPosition.y, 0);
 		
-		float distance = Vector3.Distance(launcherPosition, referencePoint.transform.position);
+		float distance = Vector3.Distance(launcherPosition, transform.position);
 
 		if (distance > maxDistance) {
-			launcherPosition = referencePoint.transform.position + Vector3.ClampMagnitude(launcherPosition - referencePoint.transform.position, maxDistance);
+			launcherPosition = transform.position + Vector3.ClampMagnitude(launcherPosition - transform.position, maxDistance);
 		}
 
-		if (launcherPosition.y > referencePoint.transform.position.y) {
-			launcherPosition = new Vector2(launcherPosition.x, referencePoint.transform.position.y);
+		if (launcherPosition.y > transform.position.y) {
+			launcherPosition = new Vector2(launcherPosition.x, transform.position.y);
 		}
 
-		gameObject.transform.position = launcherPosition;
+		current.transform.position = launcherPosition;
 	}
 
 	void LaunchEnd ()
 	{
 		launchInProgress = false;
-		gameObject.rigidbody2D.velocity = Vector2.zero;
-		gameObject.rigidbody2D.AddForce((referencePoint.transform.position - gameObject.transform.position) * forceConstant);
+		symbolInLauncher = false;
+		canSpawn = false;
+		current.rigidbody2D.velocity = Vector2.zero;
+		current.rigidbody2D.AddForce((transform.position - current.transform.position) * forceConstant);
+		Destroy (current, 5);
+		current = null;
+	}
+
+	void SpawnMathSymbol()
+	{
+		canSpawn = false;
+		symbolInLauncher = true;
+		current = Instantiate(symbols[currentIndex], transform.position, Quaternion.identity) as GameObject;
+		current.transform.position = originalSymbolPosition;
 	}
 }
