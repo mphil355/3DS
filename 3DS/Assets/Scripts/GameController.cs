26af1564fa6e2f986da0e1a272671ef9cc08ca59
Matt <mphil355@gmail.com>
2014-04-05 22:53:25 -0500

    number hit detection and simple score

---
+++ 3DS/Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+	private int goal;
+	private int current;
+
+
+	public void AddScore(int value)
+	{
+		current += value;
+		print(current);
+	}
+}//end GameController.cs
---
+++ 3DS/Assets/Scripts/Number.cs
@@ -0,0 +1,28 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+
+public class Number : MonoBehaviour 
+{
+	public int value;
+
+	private GameController gameController;
+
+	void Start()
+	{
+		gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		print ("Hit");
+		if(col.gameObject.tag == "MathSymbols")
+		{
+			gameController.AddScore(value);
+			Destroy(gameObject);
+			Destroy(col.gameObject);
+		}
+	}
+	
+}//end Number.cs
