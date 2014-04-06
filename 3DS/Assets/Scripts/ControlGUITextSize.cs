8e6500fc3a264eb80726afbbfd65669e391c34e0
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-04-06 11:28:22 -0500

    A little prettier main menu

---
+++ 3DS/Assets/Scripts/ControlGUITextSize.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlGUITextSize : MonoBehaviour {
+
+	public float sizeMultipler;
+
+	void Start () 
+	{
+		foreach (Transform child in gameObject.transform)
+		{
+			child.guiText.fontSize = Mathf.RoundToInt(Screen.height * sizeMultipler);
+			Debug.Log("Font size: " + child.guiText.fontSize.ToString());
+		}
+	}
+}
--- 3DS/Assets/Scripts/Menu.cs
+++ 3DS/Assets/Scripts/Menu.cs
@@ -5,15 +5,15 @@ public class Menu : MonoBehaviour
 {
 	void OnGUI()
 	{
-		if(GUI.Button(new Rect(10, Screen.height/2 - 100, Screen.width/4, Screen.height/10), "Start"))
+		if(GUI.Button(new Rect(Screen.width/10, Screen.height/2, Screen.width/4, Screen.height/10), "Start"))
 		{
 			Application.LoadLevel("Level");
 		}
-		if(GUI.Button(new Rect(10, Screen.height/2, Screen.width/4, Screen.height/10), "Options"))
+		if(GUI.Button(new Rect(Screen.width/2.6f, Screen.height/2, Screen.width/4, Screen.height/10), "Options"))
 		{
 			print ("options");
 		}
-		if(GUI.Button(new Rect(10, Screen.height/2 + 100, Screen.width/4, Screen.height/10), "Exit"))
+		if(GUI.Button(new Rect(Screen.width/1.5f, Screen.height/2, Screen.width/4, Screen.height/10), "Exit"))
 		{
 			print ("exit");
 		}
