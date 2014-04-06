516fb6e0d105086ce59ddfbc2ada5098a3272234
Matt Phillips <mphil355@gmail.com>
2014-04-06 12:03:14 -0500

    screen fader script and texture pause and play buttons

---
+++ 3DS/Assets/Scripts/ScreenFader.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenFader : MonoBehaviour 
+{
+	public Texture2D fadeScreen;
+
+	private bool sceneStarting;
+	private bool sceneEnding;
+	private float alpha;
+	private float fadeTime;
+
+	void Awake()
+	{
+		StartScene(3.0f);
+	}
+
+	void OnGUI()
+	{
+		SceneCheck();
+		GUI.color = new Color(1, 1, 1, alpha);
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeScreen);
+	}
+
+	void SceneCheck()
+	{		
+		if(sceneStarting)
+			FadeToClear();
+		else if(sceneEnding)
+			FadeToBlack();
+	}
+
+	void FadeToClear()
+	{
+		alpha -= Mathf.Clamp01(Time.deltaTime/fadeTime);
+		if (alpha <= 0.05f)
+		{
+			alpha = 0.0f;
+			sceneStarting = false;
+		}		
+	}
+
+	void FadeToBlack()
+	{	
+		alpha += Mathf.Clamp01(Time.deltaTime/fadeTime);
+		if (alpha >= 0.95f)
+		{
+			alpha = 1.0f;
+			sceneEnding = false;
+		}
+	}
+
+	public void StartScene(float time)
+	{
+		alpha = 1.0f;
+		sceneStarting = true;
+		sceneEnding = false;
+		fadeTime = time;
+	}
+	
+	public void EndScene(float time)
+	{
+		alpha = 0.0f;
+		sceneStarting = false;
+		sceneEnding = true;
+		fadeTime = time;
+	}
+}
+
