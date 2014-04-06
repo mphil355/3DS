a9b4bb4a069888637b99a84bb378c5b89ffb49d7
Matt <mphil355@gmail.com>
2014-04-05 23:04:13 -0500

    adjusted number and sign size, added gui text score

--- 3DS/Assets/Scripts/GameController.cs
+++ 3DS/Assets/Scripts/GameController.cs
@@ -7,10 +7,24 @@ public class GameController : MonoBehaviour
 	private int goal;
 	private int current;
 
+	private GUIText scoreText;
+
+	void Start()
+	{
+		current = 0;
+		scoreText = GetComponentInChildren<GUIText>();
+		UpdateScore();
+	}
 
 	public void AddScore(int value)
 	{
 		current += value;
 		print(current);
+		UpdateScore();
+	}
+
+	void UpdateScore()
+	{
+		scoreText.text = current.ToString();
 	}
 }//end GameController.cs
