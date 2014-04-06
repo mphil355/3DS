74680d07e659b210d3245f8b4e47f171e02baefe
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-04-06 15:07:54 -0500

    Changed logo, and build settings

---
+++ 3DS/builds/Roots/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+using Roots.Resources;
+
+namespace Roots
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
